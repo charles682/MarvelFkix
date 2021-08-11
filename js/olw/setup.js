1303aba68a486e2d8cb5ec835df44d11111ea4a0
Charles Melo <77029992+charles682@users.noreply.github.com>
2021-08-11 09:19:49 -0300

    Add files via upload

---
+++ js/olw/setup.js
@@ -0,0 +1,109 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
+
+function trocar(){
+  document.getElementById("mousemove").innerHTML = "Como filho de Odin, rei dos deuses nórdicos, Thor logo herdará o trono de Asgard de seu idoso pai. Porém, no dia de sua coroação, Thor reage com brutalidade quando os inimigos dos deuses entram no palácio violando o tratado. Como punição, Odin manda Thor para a Terra. Enquanto seu irmão Loki conspira em Asgard, Thor, agora sem seus poderes, enfrenta sua maior ameaça..";
+}
+
+
+function voltar(){
+  document.getElementById("mousemove").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar1(){
+  document.getElementById("move").innerHTML = "Como filho de Odin, rei dos deuses nórdicos, Thor logo herdará o trono de Asgard de seu idoso pai. Porém, no dia de sua coroação, Thor reage com brutalidade quando os inimigos dos deuses entram no palácio violando o tratado. Como punição, Odin manda Thor para a Terra. Enquanto seu irmão Loki conspira em Asgard, Thor, agora sem seus poderes, enfrenta sua maior ameaça..";
+}
+
+
+function voltar1(){
+  document.getElementById("move").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+
+function trocar2(){
+  document.getElementById("move2").innerHTML = "Thor está preso do outro lado do universo. Ele precisa correr contra o tempo para voltar a Asgard e parar Ragnarok, a destruição de seu mundo, que está nas mãos da poderosa e implacável vilã Hela..";
+}
+
+
+function voltar2(){
+  document.getElementById("move2").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar3(){
+  document.getElementById("move3").innerHTML = "Thor está preso do outro lado do universo. Ele precisa correr contra o tempo para voltar a Asgard e parar Ragnarok, a destruição de seu mundo, que está nas mãos da poderosa e implacável vilã Hela..";
+}
+
+
+function voltar3(){
+  document.getElementById("move3").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar4(){
+  document.getElementById("move4").innerHTML = "Depois de um inimigo reduzir o mundo de Tony Stark a destroços, o Homem de Ferro precisa aprender a confiar em seus instintos para proteger aqueles que ama, especialmente sua namorada, e lutar contra seu maior medo: o fracasso...";
+}
+
+
+function voltar4(){
+  document.getElementById("move4").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+
+function trocar5(){
+  document.getElementById("move5").innerHTML = "Vivendo escondido e longe de Betty Ross, a mulher que ama, o cientista Bruce Banner busca um meio de retirar a radiação gama que está em seu sangue. Ao mesmo tempo ele precisa fugir da perseguição do general Ross, seu grande inimigo, e da máquina militar que tenta capturá-lo, na intenção de explorar o poder que faz com que Banner se transforme no Hulk.";
+}
+
+
+function voltar5(){
+  document.getElementById("move5").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+
+function trocar6(){
+  document.getElementById("move6").innerHTML = "Da Marvel Studios vem “Doctor Strange”, a história do mundialmente famoso neurocirurgião Dr. Stephen Strange, cuja vida muda para sempre depois de um terrível acidente de carro, que o rouba do uso de suas mãos. Quando a medicina tradicional o falha, ele é forçado a procurar por cura e esperança, em um lugar improvável - um enclave misterioso conhecido como Kamar-Taj. Ele rapidamente descobre que isso não é apenas um centro de cura, mas também a linha de frente de uma batalha contra as forças ocultas invisíveis empenhadas em destruir nossa realidade. Em pouco tempo, Strange - armado com poderes mágicos recém-adquiridos - é forçado a escolher se quer retornar à sua vida de fortuna e status ou deixar tudo para trás para defender o mundo como o mais poderoso feiticeiro existente.";
+}
+
+
+function voltar6(){
+  document.getElementById("move6").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar7(){
+  document.getElementById("move7").innerHTML = "O aventureiro do espaço Peter Quill torna-se presa de caçadores de recompensas depois que rouba a esfera de um vilão traiçoeiro, Ronan. Para escapar do perigo, ele faz uma aliança com um grupo de quatro extraterrestres. Quando Quill descobre que a esfera roubada possui um poder capaz de mudar os rumos do universo, ele e seu grupo deverão proteger o objeto para salvar o futuro da galáxia.";
+}
+
+
+function voltar7(){
+  document.getElementById("move7").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar8(){
+  document.getElementById("move8").innerHTML = "Peter Parker está em uma viagem de duas semanas pela Europa, ao lado de seus amigos de colégio, quando é surpreendido pela visita de Nick Fury. Convocado para mais uma missão heroica, ele precisa enfrentar vários vilões que surgem em cidades-símbolo do continente, como Londres, Paris e Veneza, e também a aparição do enigmático Mysterio.";
+}
+
+
+function voltar8(){
+  document.getElementById("move8").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
+
+function trocar9(){
+  document.getElementById("move9").innerHTML = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.";
+}
+
+
+function voltar9(){
+  document.getElementById("move9").innerHTML = "(Sinopse passar o mouse aqui!)";
+}
